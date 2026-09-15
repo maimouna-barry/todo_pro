328c2e7928751df391cec650c995f2b379a2c825
maimouna-barry <0404thiernomaimouna@gmail.com>
2026-09-15 16:59:05 +0000

    feat: ajouter l'implementation du repository todo

---
+++ lib/features/todos/domain/repositories/todo_repository_impl.dart
@@ -0,0 +1,39 @@
+import 'package:todo_pro/features/todos/data/datasources/todo_local_data_source.dart';
+import 'package:todo_pro/features/todos/domain/entities/todo.dart';
+import 'package:todo_pro/features/todos/domain/repositories/todo_repository.dart';
+
+class TodoRepositoryImpl implements TodoRepository {
+  final TodoLocalDataSource dataSource;
+
+  TodoRepositoryImpl(this.dataSource);
+
+  @override
+  Future<List<Todo>> getTodos() {
+    return dataSource.getTodos();
+  }
+
+  @override
+  Future<Todo?> getTodo(int id) {
+    return dataSource.getTodo(id);
+  }
+
+  @override
+  Future<int> createTodo(Todo todo) {
+    return dataSource.createTodo(todo);
+  }
+
+  @override
+  Future<void> updateTodo(Todo todo) {
+    return dataSource.updateTodo(todo);
+  }
+
+  @override
+  Future<void> deleteTodo(int id) {
+    return dataSource.deleteTodo(id);
+  }
+
+  @override
+  Future<void> completedTodo(int id) {
+    return dataSource.completedTodo(id);
+  }
+}
